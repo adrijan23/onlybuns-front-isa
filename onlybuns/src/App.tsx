1f87d000baef20da4a5045be7cf2024be48285b4
Zeka5 <zekovicognjen753@gmail.com>
2024-11-10 17:34:09 +0100

    feat: fetch post

--- onlybuns/src/App.tsx
+++ onlybuns/src/App.tsx
@@ -8,73 +8,74 @@ import Chat from './components/ChatComponent/Chat';
 import Profile from './components/ProfileComponent/Profile';
 import BlogPost from './components/BlogPostComponent/BlogPost';
 import Login from './components/AuthComponents/Login';
+import Post from './components/PostComponent/Post';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
     <AuthProvider>
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<Login />} />
-                <Route
-                    path="/feed"
-                    element={
-                        <ProtectedRoute>
-                            <Feed />
-                        </ProtectedRoute>
-                    }
-                />
-                <Route
-                    path="/trends"
-                    element={
-                        <ProtectedRoute>
-                            <Trends/>
-                        </ProtectedRoute>
-                    }
-                />
-                <Route
-                    path="/map"
-                    element={
-                        <ProtectedRoute>
-                            <MapView />
-                        </ProtectedRoute>
-                    }
-                />
-                <Route
-                    path="/chat"
-                    element={
-                        <ProtectedRoute>
-                            <Chat/>
-                        </ProtectedRoute>
-                    }
-                />
-                <Route
-                    path="/blogpost"
-                    element={
-                        <ProtectedRoute>
-                            <BlogPost/>
-                        </ProtectedRoute>
-                    }
-                />
-                <Route
-                    path="/profile"
-                    element={
-                        <ProtectedRoute>
-                            <Profile />
-                        </ProtectedRoute>
-                    }
-                />
-              <Route path="/post" element={<Post authorName="John Doe"
-                authorProfileImage="https://via.placeholder.com/40"
-                postImage="https://via.placeholder.com/600"
-                description="The description of the post. Aaaaaaaaaaaaaaaaaaaaaaaaaa fenjafn jaefjaef eajkf ae jkf af."
-                likesCount={0} />} />
-            </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/feed"
+            element={
+              <ProtectedRoute>
+                <Feed />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/trends"
+            element={
+              <ProtectedRoute>
+                <Trends />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/map"
+            element={
+              <ProtectedRoute>
+                <MapView />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/chat"
+            element={
+              <ProtectedRoute>
+                <Chat />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/blogpost"
+            element={
+              <ProtectedRoute>
+                <BlogPost />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/post" element={
+            <ProtectedRoute>
+              <Post />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </Router>
     </AuthProvider>
-);
+  );
 };
 
 export default App;
